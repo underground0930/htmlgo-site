79b30470ae35f25109373559ed323254deb3d372
underground0930 <go.nishiduka.1985@gmail.com>
2020-11-27 17:26:36 +0900

    スパム対応として、フォームの項目を１つ追加

--- src/pages/contact/index.vue
+++ src/pages/contact/index.vue
@@ -6,7 +6,7 @@
         span お仕事のお問い合わせはこちらからどうぞ
       div(class="contact__body").
         <!-- class, action, methodを変更しないでください -->
-        <form class="formrun" action="https://form.run/api/v1/r/slpviahq150x5q8lxsyw5x8z" method="post" @submit.prevent="onSubmit">
+        <form class="formrun" action="https://form.run/api/v1/r/slpviahq150x5q8lxsyw5x8z" method="post">
           <!-- ↓自由に要素を追加・編集することができます -->
           <ul class="contact__list">
             <li class="contact__listChild">
@@ -24,16 +24,25 @@
               <textarea class="contact__textarea" name="お問い合わせ" data-formrun-required rows="14"></textarea>
               <div data-formrun-show-if-error="お問い合わせ">お問い合わせ入力してください</div>
             </li>
+            <li class="contact__listChild">
+              <label>スパム対策 [必須]</label>
+              <select class="contact__inputSelect" name="スパム対策" v-model="selected" data-formrun-required>
+                <option value="">令和2年11月現在の総理大臣をえらんでください</option>
+                <option value="">小泉進次郎</option>
+                <option value="">小池百合子</option>
+                <option value="">安倍晋三</option>
+                <option value="">小泉純一郎</option>
+                <option value="true">菅義偉</option>
+                <option value="">安倍昭恵</option>
+                <option value="">森田健作</option>
+                <option value="">麻生太郎</option>
+              </select>
+              <div data-formrun-show-if-error="スパム対策">総理大臣ではありません</div>
+            </li>
           </ul>
 
           <div class="contact__bot" v-if="token === '' ">ボットの可能性が検出されたため送信を中止します。</div>
           <input type="hidden" name="g-recaptcha-response" :token="token">
-
-          <!-- ボット投稿をブロックするためのタグ -->
-          <div class="_formrun_gotcha">
-            <label for="_formrun_gotcha">If you are a human, ignore this field</label>
-            <input type="text" name="_formrun_gotcha" id="_formrun_gotcha" tabindex="-1">
-          </div>
           <button type="submit" data-formrun-error-text="未入力の項目があります" data-formrun-submitting-text="送信中...">送信</button>
         </form>
       div(class="contact__back")
@@ -53,6 +62,7 @@ export default {
   },
   data() {
     return {
+      selected: '',
       token: null,
     }
   },
@@ -67,16 +77,16 @@ export default {
     }
   },
   methods: {
-    async onSubmit() {
-      try {
-        const token = await this.$recaptcha.execute('login')
-        this.token = token
-        console.log('recapcha token:', token)
-      } catch (error) {
-        this.token = ''
-        console.log('Login error:', error)
-      }
-    },
+    // async onSubmit(e) {
+    //   try {
+    //     const token = await this.$recaptcha.execute('login')
+    //     this.token = token
+    //     console.log(window.Formrun)
+    //   } catch (error) {
+    //     this.token = ''
+    //     console.log('Login error:', error)
+    //   }
+    // },
     initFormrun() {
       window.Formrun._reset()
       window.Formrun.init('.formrun')
@@ -176,6 +186,15 @@ $about_bp: 800px;
     border: 1px solid #aaa;
     resize: none;
   }
+  .contact__inputSelect {
+    width: 100%;
+    padding: var(--s2) var(--s1);
+    display: block;
+    font-size: 1.6rem;
+    border-radius: 0;
+    border: 1px solid #aaa;
+  }
+
   [data-formrun-show-if-error] {
     display: none;
   }
