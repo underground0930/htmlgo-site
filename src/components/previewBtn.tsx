08017ead4e3daf9460e4986a55630956e51b6670
underground0930 <go.nishiduka.1985@gmail.com>
2022-06-21 16:23:48 +0900

    preview削除ボタンの prefetchを削除

--- src/components/previewBtn.tsx
+++ src/components/previewBtn.tsx
@@ -4,7 +4,9 @@ import styles from 'styles/components/PreviewBtn.module.scss'
 const PreviewBtn = () => {
   return (
     <div className={styles.root}>
-      <Link href="/api/exit-preview/">プレビューCookie削除</Link>
+      <Link href="/api/exit-preview/" prefetch={false}>
+        プレビューCookie削除
+      </Link>
     </div>
   )
 }
