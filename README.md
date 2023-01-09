5f367972599b9b9eeb1cff1eed0b0826491b3cfc
underground0930 <go.nishiduka.1985@gmail.com>
2023-01-09 11:31:26 +0900

    update

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ NEXT_PUBLIC_GA_ID=xxxxxxxxxxx
 First, run the development server:
 
 ```bash
-npm run dev
+yarn dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,10 +4,10 @@ export default async function Head({}) {
   return (
     <>
       <DefaultHead
-        title={'TOP | HTMLGO'}
-        description={'WEB技術を書き連ねるサイト'}
-        image={''}
-        url={'`https://htmlgo.site/`'}
+        title='TOP | HTMLGO'
+        description='WEB技術を書き連ねるサイト'
+        image=''
+        url='https://htmlgo.site/'
         isTop={true}
         isPreview={false}
       />
