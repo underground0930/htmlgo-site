f25bde4e0e6052d2e1a0446d3f7d6399828f463d
underground0930 <go.nishiduka.1985@gmail.com>
2023-04-08 12:21:04 +0900

    formrunのアイコンを削除

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -36,11 +36,6 @@ const links = [
     icon: <IconMicrocms />,
   },
   {
-    href: 'https://form.run/ja',
-    key: 'form.run',
-    icon: <IconFormrun />,
-  },
-  {
     href: 'https://www.google.com/recaptcha/about/',
     key: 'recaptcha',
     icon: <IconRecaptcha />,
@@ -77,7 +72,9 @@ const Footer = ({}: Props) => {
             </ul>
           </dd>
         </dl>
-        <small className={className.small}>Copyright © 2016 htmlgo.site All rights reserved.</small>
+        <small className={className.small}>
+          Copyright © 2016 htmlgo.site All rights reserved.
+        </small>
       </footer>
     </>
   )
