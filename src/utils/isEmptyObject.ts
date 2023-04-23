562b074881daf5a929e367ace961021ce6d992a1
underground0930 <go.nishiduka.1985@gmail.com>
2023-04-23 23:07:37 +0900

    update

--- src/utils/isEmptyObject.ts
+++ src/utils/isEmptyObject.ts
@@ -1,4 +1,4 @@
-export function isEmptyObject(obj: any) {
+export function isEmptyObject(obj: unknown) {
   if (obj === undefined || obj === null) return false
   return Object.keys(obj).length === 0 && obj.constructor === Object
 }
