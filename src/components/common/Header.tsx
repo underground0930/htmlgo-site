60232548f4f3d7c3478287dabcab875a6a9862d8
underground0930 <go.nishiduka.1985@gmail.com>
2025-08-03 15:35:19 +0900

    headerのリンクを修正

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -13,7 +13,33 @@ const className = {
   linkStyle: ' hover:text-linkActive font-bold text-14px md:text-16px',
 }
 
-const navList = ['About', 'Works', 'Articles', 'Contact']
+const navList = [
+  {
+    label: 'Top',
+    href: '/',
+    active: (pathname: string) => pathname === '/',
+  },
+  {
+    label: 'About',
+    href: '/about',
+    active: (pathname: string) => pathname === '/about',
+  },
+  {
+    label: 'Works',
+    href: '/works',
+    active: (pathname: string) => pathname === '/works',
+  },
+  {
+    label: 'Articles',
+    href: '/articles',
+    active: (pathname: string) => pathname === '/articles',
+  },
+  {
+    label: 'Contact',
+    href: '/contact',
+    active: (pathname: string) => pathname === '/contact',
+  },
+]
 
 export const Header: React.FC = () => {
   const pathname = usePathname()
@@ -29,26 +55,17 @@ export const Header: React.FC = () => {
           </h1>
           <nav className={nav}>
             <ul className={ul}>
-              <li>
-                <Link
-                  href='/'
-                  className={(pathname === '/' ? 'text-linkActive' : '') + linkStyle}
-                >
-                  Top
-                </Link>
-              </li>
               {navList.map((item, index) => {
                 return (
-                  <li className='ml-20px' key={item + String(index)}>
+                  <li className='ml-20px' key={item.href + String(index)}>
                     <Link
                       prefetch={false}
-                      href={`/${item}`}
+                      href={item.href}
                       className={
-                        (pathname?.startsWith(`/${item}`) ? 'text-linkActive' : '') +
-                        linkStyle
+                        (item.active(pathname) ? 'text-linkActive' : '') + linkStyle
                       }
                     >
-                      {item}
+                      {item.label}
                     </Link>
                   </li>
                 )
