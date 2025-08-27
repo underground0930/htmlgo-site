f9b08573f61ee95a2825cb3a645b5f4d30999c22
underground0930 <go.nishiduka.1985@gmail.com>
2025-08-27 11:04:10 +0900

    不要なfragmentを削除

--- src/components/ui/loading-spinner.tsx
+++ src/components/ui/loading-spinner.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export const LoadingSpinner: React.FC = () => {
   return (
-    <>
-      <div className='h-10 w-10 animate-spin rounded-full border-4 border-link-active border-t-transparent' />
-    </>
+    <div className='border-link-active h-10 w-10 animate-spin rounded-full border-4 border-t-transparent' />
   )
 }
