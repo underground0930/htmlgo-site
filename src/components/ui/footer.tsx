b7b427506735d53c4d7cd933fd7f58306bb95834
underground0930 <go.nishiduka.1985@gmail.com>
2025-08-27 22:22:48 +0900

    update

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,38 +1,40 @@
+/**
+ * フッターコンポーネント
+ * サイトで使用している技術・サービスのリンクとコピーライトを表示します。
+ */
+
 'use client'
 
 import React from 'react'
-import { Image } from '@/components/utils/image'
+import { Icons, type IconsName } from '@/components/utils/icons'
 
-const links = [
+const links: {
+  href: string
+  key: IconsName
+}[] = [
   {
     href: 'https://nextjs.org/',
-    key: 'Nextjs',
-    src: '/img/footer/icon-nextjs.svg',
+    key: 'nextjs',
   },
   {
     href: 'https://vercel.com/home',
-    key: 'Vercel',
-    src: '/img/footer/icon-vercel.svg',
+    key: 'vercel',
   },
   {
     href: 'https://tailwindcss.com/',
-    key: 'Tailwind CSS',
-    src: '/img/footer/icon-tailwindcss.svg',
+    key: 'tailwindcss',
   },
   {
     href: 'https://github.com/underground0930/htmlgo-site',
     key: 'github',
-    src: '/img/footer/icon-github.svg',
   },
   {
     href: 'https://microcms.io/',
     key: 'microcms',
-    src: '/img/footer/icon-microcms.svg',
   },
   {
     href: 'https://www.google.com/recaptcha/about/',
     key: 'recaptcha',
-    src: '/img/footer/icon-recaptcha.svg',
   },
 ]
 
@@ -46,16 +48,17 @@ export const Footer: React.FC<Props> = () => {
           <dt className='mb-4'>This website powered by</dt>
           <dd>
             <ul className='flex items-center justify-center'>
-              {links.map((v) => {
+              {links.map((link) => {
+                const IconComponent = Icons[link.key]
                 return (
-                  <li className='mx-2' key={v.key}>
+                  <li className='mx-2' key={link.key}>
                     <a
                       className='relative flex w-7 items-center justify-center [&>svg]:h-auto [&>svg]:w-full'
-                      href={v.href}
+                      href={link.href}
                       target='_blank'
                       rel='noreferrer'
                     >
-                      <Image src={v.src} alt={v.key} />
+                      <IconComponent width={28} height={28} alt={link.key} />
                     </a>
                   </li>
                 )
