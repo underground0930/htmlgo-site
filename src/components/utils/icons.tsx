610c65c5662b5549963f9b11592e9bc848e510cb
underground0930 <go.nishiduka.1985@gmail.com>
2025-08-31 11:47:22 +0900

    refactor: update prettier config and icons

--- src/components/utils/icons.tsx
+++ src/components/utils/icons.tsx
@@ -15,53 +15,26 @@ type IconProps = {
   width?: number
   height?: number
   alt?: string
+  color?: string
 }
 
 /**
  * アイコン管理
  */
 export const Icons = {
-  react: ({ className, size = 24 }: IconProps) => (
-    <FaReact className={className} size={size} />
-  ),
-  typescript: ({ className, size = 24 }: IconProps) => (
-    <SiTypescript className={className} size={size} />
-  ),
-  nextjs: ({ className, size = 24 }: IconProps) => (
-    <SiNextdotjs className={className} size={size} />
-  ),
-  vercel: ({ className, size = 24 }: IconProps) => (
-    <SiVercel className={className} size={size} />
-  ),
-  tailwindcss: ({ className, size = 24 }: IconProps) => (
-    <SiTailwindcss className={className} size={size} />
-  ),
-  github: ({ className }: IconProps) => (
-    <VscGithub className={className} width={24} height={24} />
-  ),
-  wordpress: ({ className, width = 24, height = 24 }: IconProps) => (
-    <FaWordpress className={className} width={width} height={height} />
-  ),
-  home: ({ className, size = 18 }: IconProps) => (
-    <RxHome className={className} size={size} />
-  ),
+  react: ({ size = 24, ...props }: IconProps) => <FaReact {...props} size={size} />,
+  typescript: ({ size = 24, ...props }: IconProps) => <SiTypescript {...props} size={size} />,
+  nextjs: ({ size = 24, ...props }: IconProps) => <SiNextdotjs {...props} size={size} />,
+  vercel: ({ size = 24, ...props }: IconProps) => <SiVercel {...props} size={size} />,
+  tailwindcss: ({ size = 24, ...props }: IconProps) => <SiTailwindcss {...props} size={size} />,
+  github: ({ size = 24, ...props }: IconProps) => <VscGithub {...props} size={size} />,
+  wordpress: ({ size = 24, ...props }: IconProps) => <FaWordpress {...props} size={size} />,
+  home: ({ size = 18, ...props }: IconProps) => <RxHome {...props} size={size} />,
   microcms: ({ className, width = 24, height = 24, alt = 'microCMS' }: IconProps) => (
-    <Image
-      src='/img/footer/icon-microcms.svg'
-      alt={alt}
-      width={width}
-      height={height}
-      className={className}
-    />
+    <Image src='/img/footer/icon-microcms.svg' alt={alt} width={width} height={height} className={className} />
   ),
   recaptcha: ({ className, width = 24, height = 24, alt = 'reCAPTCHA' }: IconProps) => (
-    <Image
-      src='/img/footer/icon-recaptcha.svg'
-      alt={alt}
-      width={width}
-      height={height}
-      className={className}
-    />
+    <Image src='/img/footer/icon-recaptcha.svg' alt={alt} width={width} height={height} className={className} />
   ),
 }
 
