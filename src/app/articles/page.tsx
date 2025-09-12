9468cec6d8ac440b77eb0876ef8ab16295b8a45f
underground0930 <go.nishiduka.1985@gmail.com>
2025-09-12 17:17:14 +0900

    articles: page-child

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -4,13 +4,15 @@
  */
 
 import { Metadata } from 'next'
-import { ArticlesPageComponent } from './components/articles-page'
+import { PageChild } from './components/page-child'
 import { nextMetaData } from '@/libs/next-metadata'
+import { fetchArticles } from './libs/fetch-articles'
 
 const description = '色々なブログの記事のフィードをまとめたものです'
 
-export default function Page() {
-  return <ArticlesPageComponent />
+export default async function Page() {
+  const result = await fetchArticles({ params: { page: '1' } })
+  return <PageChild {...result} />
 }
 
 export const metadata: Metadata = {
